9ca35e47fa1450e038125e39706c7f14a09516c7
WissemChihaoui <chihahoui10@gmail.com>
2025-12-03 12:42:32 +0100

    fixes 03/12 config

--- src/config-global.js
+++ src/config-global.js
@@ -4,7 +4,7 @@ export const CONFIG = {
   appName: "SPC",
   storageKey: import.meta.env.STORAGE_KEY || "spc_access_token",
   serverUrl: import.meta.env.SERVER_URL || "https://spc.emc1001.online/",
-  frontUrl: import.meta.env.FRONT_URL || "http://localhost:5173",
+  frontUrl: import.meta.env.FRONT_URL || "https://front-spc.emc1001.online",
   stripePublicKey:"pk_test_51SV9rO8t0y3r1ZkE1Fcc7Kgdp4pTqyOBCrFYh37A8nkiaOympPOEamYSP7u4JwCb6eR9On6kKPdpNCnAR02rNLCW00VMVoaBdT",
 
   auth: {
